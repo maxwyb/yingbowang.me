121a0a7b72ce8bf46f67879d39a84194408ae3f3
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-09-04 09:30:03 -0700

    Add database and model classes.

--- helloweb/helloweb/settings.py
+++ helloweb/helloweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'welcome.apps.WelcomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'helloweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'personal-website',
+        'USER': 'postgre',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
